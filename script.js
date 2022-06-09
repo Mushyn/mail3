e8ae4d3812fc3471b7854edc52c9c79676397d5a
Olivier Magnin <ofmagnin@gmail.com>
2022-06-09 12:20:35 +0200

    listener bouton en place

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ for(var i=0; i<document.getElementsByClassName('trash').length; i++){
     )
 
 }
+
+let ecouteBouton = document.getElementById('boutonAddMessage');
+let messageAEnvoyer = document.getElementById('newMessage');
+let maRowCustom='';
+
+ecouteBouton.addEventListener('click',
+    function () {
+        if (messageAEnvoyer.value !== ''){
+        console.log(messageAEnvoyer.value);
+        document.getElementById('newMessage').value='';
+        }
+    }
+)
+
+const createMessage = (texte) {
+
+    let expediteurName = 'Olivier Magnin';
+    maRowCustom = "<div class='row'><img class='avatar' src='avatar-1.jpg'><div><h6>" + expediteurName + "</h6><p>" + texte + "</p></div><img class='trash' src='trash.png'></div>";
+
+}
